26fa58cdd7cd5ca618229d6fd2101d0714ea20f9
mshults-4xxi <mshults@4xxi.com>
2022-11-23 22:56:46 +0500

    add mock data

---
+++ mock-data/activityTypes.ts
@@ -0,0 +1,20 @@
+
+export const mockData = [{
+  name: 'Work',
+  color: '#48bd54'
+}, {
+  name: 'Break',
+  color: '#ffbc48'
+}, {
+  name: 'Training',
+  color: '#5266b9'
+}, {
+  name: 'Sick leave',
+  color: '#c371d3'
+}, {
+  name: 'Vacation',
+  color: '#b3b3b3'
+}, {
+  name: 'Empty',
+  color: 'transparent'
+}];
---
+++ mock-data/intervals.ts
@@ -0,0 +1,79 @@
+
+import { ScheduleData } from '../src/models';
+
+const schedule: ScheduleData = {
+  id: '123123',
+  userName: '123123',
+  list: [
+    {
+    start: 0,
+    end: 60,
+    type: 'Break',
+    id: '111'
+  },
+  {
+    start: 120,
+    end: 180,
+    type: 'Work',
+    id: '222'
+  }
+  , {
+    start: 240,
+    end: 300,
+    type: 'Work',
+    id: '333'
+  }
+  , {
+    start: 360,
+    end: 720,
+    type: 'Work',
+    id: '444'
+  }
+]
+};
+
+const names = [
+  'Cleo Bowes Cleo Bowes Cleo Bowes',
+  'Jagdeep Cairns',
+  'Anisa Olsen',
+  'Asiyah Wilson',
+  'Kelsi Stevenson',
+  'Benas Stott',
+  'Caleb Hubbard',
+  'Nikola Stone',
+  'Shauna Mckenzie',
+  'Shakira Hess',
+  'Ed Cohen',
+  'Denny Cochran',
+  'Irfan Mueller',
+  'Krista Rodriguez',
+  'Fannie Dalton',
+  'Alix Gould',
+  'Kajetan Robbins',
+  'Cheyenne Watts',
+  'Eshaal Nash',
+  'Jena Bowman',
+  'Seb Irwin',
+  'Luella Simpson',
+  'Madeeha Friedman',
+  'Bree Whitworth',
+  'Horace Burch',
+  'Wilfred Beech',
+  'India Jenkins',
+  'Mared Morales',
+  'Izabel Hobbs',
+  'Lincoln Richard',
+];
+
+const generateUsers = () => {
+  const res: ScheduleData[] = [];
+
+  for (let name of names) {
+    res.push({ ...schedule, userName: name, id: name })
+  }
+
+  return res;
+};
+
+
+export const mockData: ScheduleData[] = generateUsers();
